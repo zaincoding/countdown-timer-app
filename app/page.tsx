a1077c721fe85405618042e42cd5609cb81707c5
zain <zainmd.htm@gmail.com>
2024-09-11 21:33:32 +0500

    Project Count Down Timer App

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+// export default function Home(){
+//     return(
+//         <h1>Hello World</h1>
+//     )
+// }
+
+import CountDownTimer from "@/components/count-down";
+                                        
+
+export default function Home() {
+  return (
+    <div>
+      <CountDownTimer />
+    </div>
+  );
+}
